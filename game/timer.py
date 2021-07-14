932fbdef5a5c2149093049da039b7db7294ddf64
PromiseGithub <73972260+PromiseGithub@users.noreply.github.com>
2021-07-14 11:06:08 -0600

    timer update

--- game/timer.py
+++ game/timer.py
@@ -9,9 +9,19 @@ class Timer(Actor):
                 self.center_x = center_x
                 self.center_y = center_y
                 self.scale = constants.TILE_SCALE
+                self.total_time = 0.0
         
         def draw(self):
-                arcade.draw_text("timer", 100, 100, arcade.color.BLUE, 12)
+                minutes = int(self.total_time) // 60
+                seconds = int(self.total_time) % 60
+                output = f"Time: {minutes:02d}:{seconds:02d}"
+                arcade.draw_text(output, 10, 10, arcade.color.BLUE, 20)
+
+        def on_update(self, delta_time):
+                """
+                All the logic to move, and the game logic goes here.
+                """
+                self.total_time += delta_time 
 
 
 
@@ -44,9 +54,3 @@ class Timer(Actor):
 
         # Output the timer text.
 #        arcade.draw_text(output, 600, 480, arcade.color.BLACK, 15)
-
-#    def on_update(self, delta_time):
-#        """
-#        All the logic to move, and the game logic goes here.
-#        """
-#        self.total_time += delta_time 
