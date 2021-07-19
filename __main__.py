5ed77b36a54e84c69883daceba1c120c35ae374a
Riley Hansen <gridironhero@gmail.com>
2021-07-19 13:43:59 -0600

    Notes

--- __main__.py
+++ __main__.py
@@ -15,3 +15,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+        # #1 create label Class
+    #   make sure you can set the text
+    # #2Inside Check timer action
+    # #3Create an instance of Label to to cast
+    # #4Set the text to "Game Over"
+    # #5Add the Label class to the cast
+    # #6Inside the draw_actors_action make sure that it is being drawn
+
+    # #Add a Game Starting Menu ^^
